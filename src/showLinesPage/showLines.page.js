2ce2ff57d7e9812e20a077da5e76e99253928a2a
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-06-23 23:41:25 -0700

    began work on reorganization

---
+++ src/showLinesPage/showLines.page.js
@@ -0,0 +1,226 @@
+import React from 'react';
+import { View, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { ScreenOrientation } from 'expo';
+
+const { width: winWidth, height: winHeight } = Dimensions.get('window');
+
+export default class ShowLinesPage extends React.Component {
+  state = {
+    curLine: 0,
+    curCharacters: ['FJ:', 'F):', 'F]:'],
+    linesByCharacter: {},
+  };
+
+  componentDidMount() {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+  };
+
+  addToCharacterLines = (curName, curWords, screenFactor) => {
+    let rects = [];
+    for (let line of curWords) {
+      let minTop = line[0].Top;
+      let minLeft = line[0].Left;
+      let maxLeftWidth = line[0].Left + line[0].Width;
+      let maxTopHeight = line[0].Top + line[0].Height;
+      for (let i = 1; i < line.length; i++) {
+        if (line[i].Top < minTop) {
+          minTop = line[i].Top;
+        }
+        if (line[i].Left < minLeft) {
+          minLeft = line[i].Left;
+        }
+        if (line[i].Left + line[i].Width > maxLeftWidth) {
+          maxLeftWidth = line[i].Left + line[i].Width;
+        }
+        if (line[i].Top + line[i].Height > maxTopHeight) {
+          maxTopHeight = line[i].Top + line[i].Height
+        }
+      }
+
+      // Normalizes ocr space coords to window coords
+      minTop *= screenFactor;
+      minLeft *= screenFactor;
+      maxLeftWidth *= screenFactor;
+      maxTopHeight *= screenFactor;
+
+      let rect = {
+        top: minTop,
+        left: minLeft,
+        width: maxLeftWidth - minLeft,
+        height: maxTopHeight - minTop,
+      };
+      rects.push(rect);
+    }
+
+    console.log(curName);
+    if (curName in this.state.linesByCharacter) {
+      this.state.linesByCharacter[curName] = this.state.linesByCharacter[curName].concat(rects);
+    } else { 
+      this.state.linesByCharacter[curName] = rects;
+    }
+  };
+
+  isName = (text) => {
+    return text.length >= 2 && text === text.toUpperCase();
+  };
+
+  cropLines = async (capture) => {
+    console.log('GET OCR DATA');
+    //const ocrData = await this.getImageText(capture);
+
+    console.log('CROPPED LINES START');
+    //const textOverlay = ocrData.overlay;
+    //const orientation = parseInt(ocrData.orientation);
+    const textOverlay = data;
+    const orientation = 0;
+    console.log(textOverlay);
+    
+    if ('error' in textOverlay) {
+      console.log('ERROR');
+      console.log(textOverlay.error);
+      return;
+    }
+    // TODO(URGENT): handle the possible error from textOverlay
+    //                Also: what if textOverlay is empty??
+
+    // TODO: Adjust code so it adapts to whether the size of image has been manipulated to be under 1 kb
+    //       Do this either by cropping the original image, or by multiplying the textOverlay values by a factor
+
+    let cropY = [];
+    let top = 0;
+    let bot = 0;
+    const isSideways = orientation == 270 || orientation == 90;
+    const imWidth = isSideways ? capture.height : capture.width;
+    const imHeight = isSideways ? capture.width : capture.height;
+
+    const screenFactor = winHeight/imWidth;
+
+    let curName = '';
+    let curWords = [];
+    for (let line of textOverlay.Lines) {
+      const firstWord = line.Words[0].WordText;
+      if (this.isName(firstWord)) {
+        // If the current line has a name in it
+        if (curName.length > 0) {
+          if (curWords.length > 0)
+            this.addToCharacterLines(curName, curWords, screenFactor);
+          curName = '';
+          curWords = [];
+        }
+
+        bot = line.MinTop * screenFactor;
+        cropY.push({top, bot});
+        top = bot;
+
+        curName = firstWord;
+
+        let i;
+        for (i = 1; i < line.Words.length; i++) {
+          if (this.isName(line.Words[i].WordText))
+            curName += ' ' + line.Words[i].WordText;
+          else
+            break;
+        }
+
+        let words = line.Words.slice(i);
+        if (words.length > 0)
+          curWords.push(words);
+      } else {
+        let words = line.Words;
+        if (words.length > 0)
+          curWords.push(words);
+      }
+    }
+    this.addToCharacterLines(curName, curWords, screenFactor);
+    
+    cropY.push({top, bot: imHeight*screenFactor});
+
+    this.setState({ 
+      croppedLines: [...this.state.croppedLines, cropY]
+    });
+  }
+
+  nextLine = () => {
+    const { croppedLines, curLine } = this.state;
+    const curCropList = croppedLines[0];
+    if (curLine + 1 < curCropList.length) 
+      this.setState({ curLine: curLine + 1 });
+  };
+
+  prevLine = () => {
+    const { curLine } = this.state;
+    if (curLine - 1 >= 0) 
+      this.setState({ curLine: curLine - 1 });
+  };
+
+  render() {
+    const { croppedLines, curLine, curCharacters, linesByCharacter } = this.state;
+    const curCropList = croppedLines[0];
+
+    return (
+      <React.Fragment>
+        <StatusBar hidden={true} />
+        { croppedLines.length > 0 && 
+          <View>
+            <Image 
+              source={{uri: captures[0].uri}}
+              style={ 
+                    {  width: winHeight, 
+                        height: captures[0].height * winHeight/captures[0].width,
+                        position: 'absolute',  
+                        top: winWidth/2 - (curCropList[curLine].bot-curCropList[curLine].top)/2 - curCropList[curLine].top,
+                    }}
+            />
+            <LinearGradient
+              colors={['rgba(0, 0, 0, 0.9)', 'rgba(0, 0, 0, 0.5)']}
+              style={{
+                position: 'absolute',
+                left: 0,
+                top: 0,
+                width: winHeight,
+                height: (winWidth - (curCropList[curLine].bot-curCropList[curLine].top)) / 2,
+              }}
+            >
+              <TouchableOpacity onPress={this.prevLine} style={{flex: 1}} />
+            </LinearGradient>
+            <LinearGradient
+              colors={['rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0, 0.9)']}
+              style={{
+                position: 'absolute',
+                left: 0,
+                top: (winWidth - (curCropList[curLine].bot-curCropList[curLine].top)) / 2 + (curCropList[curLine].bot-curCropList[curLine].top),
+                width: winHeight,
+                height: (winWidth - (curCropList[curLine].bot-curCropList[curLine].top)) / 2,
+              }}
+            >
+              <TouchableOpacity onPress={this.nextLine} style={{flex: 1}} />
+            </LinearGradient>
+
+            {
+              curCharacters.map((name, i) => {
+                if (name in linesByCharacter) {
+                  return linesByCharacter[name].map((rect, j) => (
+                    <View 
+                      key={i + '_' + j}
+                      style={{
+                        backgroundColor: 'black',
+                        position: 'absolute',
+                        width: rect.width,
+                        height: rect.height,
+                        top: winWidth/2 - (curCropList[curLine].bot-curCropList[curLine].top)/2 - curCropList[curLine].top + rect.top,
+                        left: rect.left
+                      }}
+                    />
+                  ));
+                }
+                return null;
+              })
+              
+            }
+          </View>
+        }
+      </React.Fragment>
+    );
+  };
+}
--- src/uploadPage/upload.page.js
+++ src/uploadPage/upload.page.js
@@ -1,34 +1,31 @@
 import React from 'react';
-import { Text, View, Image, Dimensions, StatusBar, Button, TouchableOpacity } from 'react-native';
+import { Text, View, Image, StatusBar } from 'react-native';
 import * as ImageManipulator from 'expo-image-manipulator';
 import Config from 'react-native-config';
 import * as FileSystem from 'expo-file-system';
 import { ScreenOrientation } from 'expo';
-import { LinearGradient } from 'expo-linear-gradient';
 
 import styles from '../styles';
 import data from './testdata';
 
-const { width: winWidth, height: winHeight } = Dimensions.get('window');
-
 // TODO: Add zooming and horizontal panning
+// TODO: Add selection box that allows you to choose which character to black lines out for
 
 export default class UploadPage extends React.Component {
   state = {
-    croppedLines: [],
-    curLine: 0,
-    curCharacters: ['FJ:', 'F):', 'F]:'],
-    linesByCharacter: {},
+    ocrInfo: [],
+    captures: [],
   };
 
   componentDidMount() {
-    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
-    const captures = this.props.navigation.getParam('captures', []);
-    if (captures.length > 0)
-      this.cropLines(captures[0]);
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+    this.setState({ captures: this.props.navigation.getParam('captures', []) });
+    if (this.state.captures.length > 0)
+      this.getOCRInfo();
   };
 
   resizeImage = async (capture) => {
+    // Resize image so it's under the file size limit for OCR.space
     let compressIndex = 0;
     const compressValues = [{compress: 1, width: 0}, 
                             {compress: 0.9, width: 0},
@@ -73,6 +70,7 @@ export default class UploadPage extends React.Component {
   };
 
   getImageText = async (capture) => {
+    // Use the OCR.space API to get the image text
     const base64Image = await this.resizeImage(capture);
 
     let formData = new FormData();
@@ -104,213 +102,18 @@ export default class UploadPage extends React.Component {
     return textOverlay;
   };
 
-  addToCharacterLines = (curName, curWords, screenFactor) => {
-    let rects = [];
-    for (let line of curWords) {
-      let minTop = line[0].Top;
-      let minLeft = line[0].Left;
-      let maxLeftWidth = line[0].Left + line[0].Width;
-      let maxTopHeight = line[0].Top + line[0].Height;
-      for (let i = 1; i < line.length; i++) {
-        if (line[i].Top < minTop) {
-          minTop = line[i].Top;
-        }
-        if (line[i].Left < minLeft) {
-          minLeft = line[i].Left;
-        }
-        if (line[i].Left + line[i].Width > maxLeftWidth) {
-          maxLeftWidth = line[i].Left + line[i].Width;
-        }
-        if (line[i].Top + line[i].Height > maxTopHeight) {
-          maxTopHeight = line[i].Top + line[i].Height
-        }
-      }
-
-      // Normalizes ocr space coords to window coords
-      minTop *= screenFactor;
-      minLeft *= screenFactor;
-      maxLeftWidth *= screenFactor;
-      maxTopHeight *= screenFactor;
-
-      let rect = {
-        top: minTop,
-        left: minLeft,
-        width: maxLeftWidth - minLeft,
-        height: maxTopHeight - minTop,
-      };
-      rects.push(rect);
+  getOCRInfo = async () => {
+    for (let capture of this.state.captures) {
+      const textOverlay = await getImageText(capture);
+      this.state.ocrInfo.push(textOverlay);
     }
-
-    console.log(curName);
-    if (curName in this.state.linesByCharacter) {
-      this.state.linesByCharacter[curName] = this.state.linesByCharacter[curName].concat(rects);
-    } else { 
-      this.state.linesByCharacter[curName] = rects;
-    }
-  };
-
-  isName = (text) => {
-    return text.length >= 2 && text === text.toUpperCase();
-  };
-
-  cropLines = async (capture) => {
-    console.log('GET OCR DATA');
-    //const ocrData = await this.getImageText(capture);
-
-    console.log('CROPPED LINES START');
-    //const textOverlay = ocrData.overlay;
-    //const orientation = parseInt(ocrData.orientation);
-    const textOverlay = data;
-    const orientation = 0;
-    console.log(textOverlay);
-    
-    if ('error' in textOverlay) {
-      console.log('ERROR');
-      console.log(textOverlay.error);
-      return;
-    }
-    // TODO(URGENT): handle the possible error from textOverlay
-    //                Also: what if textOverlay is empty??
-
-    // TODO: Adjust code so it adapts to whether the size of image has been manipulated to be under 1 kb
-    //       Do this either by cropping the original image, or by multiplying the textOverlay values by a factor
-
-    let cropY = [];
-    let top = 0;
-    let bot = 0;
-    const isSideways = orientation == 270 || orientation == 90;
-    const imWidth = isSideways ? capture.height : capture.width;
-    const imHeight = isSideways ? capture.width : capture.height;
-
-    const screenFactor = winHeight/imWidth;
-
-    let curName = '';
-    let curWords = [];
-    for (let line of textOverlay.Lines) {
-      const firstWord = line.Words[0].WordText;
-      if (this.isName(firstWord)) {
-        // If the current line has a name in it
-        if (curName.length > 0) {
-          if (curWords.length > 0)
-            this.addToCharacterLines(curName, curWords, screenFactor);
-          curName = '';
-          curWords = [];
-        }
-
-        bot = line.MinTop * screenFactor;
-        cropY.push({top, bot});
-        top = bot;
-
-        curName = firstWord;
-
-        let i;
-        for (i = 1; i < line.Words.length; i++) {
-          if (this.isName(line.Words[i].WordText))
-            curName += ' ' + line.Words[i].WordText;
-          else
-            break;
-        }
-
-        let words = line.Words.slice(i);
-        if (words.length > 0)
-          curWords.push(words);
-      } else {
-        let words = line.Words;
-        if (words.length > 0)
-          curWords.push(words);
-      }
-    }
-    this.addToCharacterLines(curName, curWords, screenFactor);
-    
-    cropY.push({top, bot: imHeight*screenFactor});
-
-    this.setState({ 
-      croppedLines: [...this.state.croppedLines, cropY]
-    });
-  }
-
-  nextLine = () => {
-    const { croppedLines, curLine } = this.state;
-    const curCropList = croppedLines[0];
-    if (curLine + 1 < curCropList.length) 
-      this.setState({ curLine: curLine + 1 });
-  };
-
-  prevLine = () => {
-    const { curLine } = this.state;
-    if (curLine - 1 >= 0) 
-      this.setState({ curLine: curLine - 1 });
   };
 
   render() {
-    const captures = this.props.navigation.getParam('captures', []);
-    const { croppedLines, curLine, curCharacters, linesByCharacter } = this.state;
-    const curCropList = croppedLines[0];
-
     return (
       <React.Fragment>
-        <StatusBar hidden={true} />
-        <View>
-          { croppedLines.length > 0 && 
-          <View>
-            <Image 
-              source={{uri: captures[0].uri}}
-              style={ 
-                    {  width: winHeight, 
-                        height: captures[0].height * winHeight/captures[0].width,
-                        position: 'absolute',  
-                        top: winWidth/2 - (curCropList[curLine].bot-curCropList[curLine].top)/2 - curCropList[curLine].top,
-                    }}
-            />
-            <LinearGradient
-              colors={['rgba(0, 0, 0, 0.9)', 'rgba(0, 0, 0, 0.5)']}
-              style={{
-                position: 'absolute',
-                left: 0,
-                top: 0,
-                width: winHeight,
-                height: (winWidth - (curCropList[curLine].bot-curCropList[curLine].top)) / 2,
-              }}
-            >
-              <TouchableOpacity onPress={this.prevLine} style={{flex: 1}} />
-            </LinearGradient>
-            <LinearGradient
-              colors={['rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0, 0.9)']}
-              style={{
-                position: 'absolute',
-                left: 0,
-                top: (winWidth - (curCropList[curLine].bot-curCropList[curLine].top)) / 2 + (curCropList[curLine].bot-curCropList[curLine].top),
-                width: winHeight,
-                height: (winWidth - (curCropList[curLine].bot-curCropList[curLine].top)) / 2,
-              }}
-            >
-              <TouchableOpacity onPress={this.nextLine} style={{flex: 1}} />
-            </LinearGradient>
-
-            {
-              curCharacters.map((name, i) => {
-                if (name in linesByCharacter) {
-                  return linesByCharacter[name].map((rect, j) => (
-                    <View 
-                      key={i + '_' + j}
-                      style={{
-                        backgroundColor: 'black',
-                        position: 'absolute',
-                        width: rect.width,
-                        height: rect.height,
-                        top: winWidth/2 - (curCropList[curLine].bot-curCropList[curLine].top)/2 - curCropList[curLine].top + rect.top,
-                        left: rect.left
-                      }}
-                    />
-                  ));
-                }
-                return null;
-              })
-              
-            }
-          </View>
-          }
-        </View>
+        <StatusBar hidden={false} />
+        <Text>hello there!</Text>
       </React.Fragment>
     );
   };
