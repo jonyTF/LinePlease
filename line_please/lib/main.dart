7188eb49011c2210215a420cc8b221114b4df3bb
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-12-18 23:01:30 -0800

    Added mlkit and basic text recognition

--- line_please/lib/main.dart
+++ line_please/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -48,11 +51,63 @@ class ScriptListPage extends StatelessWidget {
           return ListTile(
             title: Text(scripts[index].title),
             onTap: () {
-              
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ScriptDetailsPage(script: scripts[index]),
+                ),
+              );
             },
           );
         },
       ),
     );
+  }
+}
+
+class ScriptDetailsPage extends StatelessWidget {
+  final Script script;
+
+  ScriptDetailsPage({Key key, @required this.script}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    _getTextData();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(script.title),
+      ),
+      body: ListView(
+        children: [
+          Text('Page count: ${script.pageCount}'),
+          Image.asset('assets/img/test_edited.jpg'),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _getTextData() async {
+    final File imageFile = await ImagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    //final File imageFile = new File('assets/img/test_edited.jpg');
+    //final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFilePath('assets/img/test_edited.jpg');
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
+
+    final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+    final VisionText visionText = await textRecognizer.processImage(visionImage);
+
+    print('READY to print!');
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement element in line.elements) {
+          print(element.text);
+        }
+      }
+    }
+
+    textRecognizer.close();
   }
 }
