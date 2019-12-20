9a8a00abf710eab11a55137de73f7df96abae986
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-12-19 17:20:10 -0800

    bounding boxes

--- line_please/lib/main.dart
+++ line_please/lib/main.dart
@@ -65,14 +65,24 @@ class ScriptListPage extends StatelessWidget {
   }
 }
 
-class ScriptDetailsPage extends StatelessWidget {
+class ScriptDetailsPageState extends State<ScriptDetailsPage> {
   final Script script;
+  File imageFile;
+  final List<Rect> textData = <Rect>[];
+  int im_width;
+  int im_height;
 
-  ScriptDetailsPage({Key key, @required this.script}) : super(key: key);
+  ScriptDetailsPageState({@required this.script});
 
   @override
   Widget build(BuildContext context) {
-    _getTextData();
+    if (imageFile == null)
+      _getTextData();
+
+    Widget imageOverlay = CustomPaint(
+      foregroundPainter: ImageOverlayPainter(textData: textData, im_width: im_width, im_height: im_height),
+      child: imageFile != null ? Image.file(imageFile) : Image.asset('assets/img/test.jpg'),
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -81,33 +91,90 @@ class ScriptDetailsPage extends StatelessWidget {
       body: ListView(
         children: [
           Text('Page count: ${script.pageCount}'),
-          Image.asset('assets/img/test_edited.jpg'),
+          imageOverlay,
         ],
       ),
     );
   }
 
   Future<void> _getTextData() async {
-    final File imageFile = await ImagePicker.pickImage(
+    final File f = await ImagePicker.pickImage(
       source: ImageSource.gallery,
     );
+    final decodedImage = await decodeImageFromList(f.readAsBytesSync());
+    print('WIDTH: ${decodedImage.width}, HEIGHT: ${decodedImage.height}');
 
     //final File imageFile = new File('assets/img/test_edited.jpg');
     //final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFilePath('assets/img/test_edited.jpg');
-    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(f);
 
     final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
     final VisionText visionText = await textRecognizer.processImage(visionImage);
 
-    print('READY to print!');
+    print(visionText.text);
+
     for (TextBlock block in visionText.blocks) {
       for (TextLine line in block.lines) {
-        for (TextElement element in line.elements) {
-          print(element.text);
-        }
+        final Rect boundingBox = line.boundingBox;
+        textData.add(boundingBox);
       }
     }
 
     textRecognizer.close();
+
+    setState(() {
+      imageFile = f;
+      im_width = decodedImage.width;
+      im_height = decodedImage.height;
+    });
+  }
+}
+
+class ScriptDetailsPage extends StatefulWidget {
+  final Script script;
+
+  ScriptDetailsPage({@required this.script});
+
+  @override
+  ScriptDetailsPageState createState() {
+    return ScriptDetailsPageState(script: script);
+  }
+}
+
+class ImageOverlayPainter extends CustomPainter {
+  final int im_width;
+  final int im_height;
+  final List<Rect> textData;
+
+  ImageOverlayPainter({@required this.textData, @required this.im_width, @required this.im_height});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.red;
+
+    final ratio_w = size.width/im_width;
+    final ratio_h = size.height/im_height;
+    //print('RATIO_W: $ratio_w, RATIO_H: $ratio_h');
+
+    for (Rect r in textData) {
+      //print('OLD_RECT: $r');
+      final left = r.left * ratio_w;
+      final right = r.right * ratio_w;
+      final top = r.top * ratio_h;
+      final bottom = r.bottom * ratio_h;
+      //Rect new_rect = Rect.fromLTRB(left, top, right, bottom);
+      //print('NEW_RECT : $new_rect');
+      
+      canvas.drawLine(Offset(left, top), Offset(right, top), paint);
+      canvas.drawLine(Offset(left, bottom), Offset(right, bottom), paint);
+      canvas.drawLine(Offset(left, top), Offset(left, bottom), paint);
+      canvas.drawLine(Offset(right, top), Offset(right, bottom), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
